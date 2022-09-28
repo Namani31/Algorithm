8dadddc29867a76c701e26e76d8041b0b949e062
NaeRuMi <71462755+yunjikim-31@users.noreply.github.com>
2022-09-28 20:45:24 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 296 ms, Memory: 17852 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -6,11 +6,10 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int num = input.nextInt();
 
-        for (int rowLoop = 1; rowLoop <= num; rowLoop++) {
-            for (int colLoop = 1; colLoop <= rowLoop; colLoop++) {
+        for (int rowNum = 0; rowNum < num; rowNum++) {  // 가로 찍기
+            for (int colNum = 0; colNum <= rowNum; colNum++)    // 세로 찍기, colNum < rowNum 을 하면 맨 처음에 안 찍고 두번째줄부터 찍음
                 System.out.print("*");
-            }
-            System.out.print("\n");
+            System.out.print("\n"); // 별 다 찍고나면 줄 개행
         }
     }
 }
