7a36d5410e93b3d0d34a377c6c946b4d62e97188
NaeRuMi <71462755+yunjikim-31@users.noreply.github.com>
2022-10-26 16:33:33 +0900

    [level 1] Title: 약수의 합, Time: 0.03 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -1,16 +1,24 @@
-class Solution {
+class Solution
+{
     public int solution(int num) {
-        if (num < 0 || num > 3000)
-            return -1;
-
         int answer = 0;
 
-        // 모든 약수의 합이므로 절반까지, 소수 중 2가 가장 작으니까
-        for(int loop = 1; loop <= num / 2; loop++){
-            if(num % loop == 0)
-                answer += loop;
+        for (int loop = 1; loop <= Math.sqrt(num); loop++) {
+            // 나누어 떨어질 경우
+            if (num % loop == 0)
+            {
+                // n의 제곱근일 경우
+                if (loop * loop == num) {
+                    answer += loop;
+                }
+
+                // n의 제곱근이 아닐 경우
+                else {
+                    answer += loop;
+                    answer += num / loop;
+                }
+            }
         }
-        // 약수 중에 자기 자신 포함
-        return answer + num;
+        return answer;
     }
 }
