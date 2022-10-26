c839df695b0b8e086ce45405d365491395b29cc0
NaeRuMi <71462755+yunjikim-31@users.noreply.github.com>
2022-10-26 16:58:14 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -6,10 +6,9 @@ public class Solution {
 
         while (n != 0) {
             answer += (n % 10);
-            n = n/10;
+            n /= 10;
         }
 
-        System.out.println(answer);
         return answer;
     }
 }
