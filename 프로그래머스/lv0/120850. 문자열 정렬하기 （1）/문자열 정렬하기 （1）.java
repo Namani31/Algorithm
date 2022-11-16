4fd586a141fcfc35e9839e820617ec12866aa903
NaeRuMi <71462755+yunjikim-31@users.noreply.github.com>
2022-11-16 23:46:00 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 2.78 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -1,10 +1,10 @@
 import java.util.*;
-import java.util.stream.Stream;
 
 class Solution {
     public int[] solution(String my_string) {
-        String intStr = my_string.replaceAll("[^0-9]", "");
-        int[] answer = Arrays.stream(intStr.split("")).mapToInt(Integer::parseInt).toArray();
+        String removeChar= my_string.replaceAll("[^0-9]", ""); // 숫자만 남기고 문자열 제거
+        int[] answer = Arrays.stream(removeChar.split("")).mapToInt(Integer::parseInt).toArray();
+        // removeChar에 있는 문자열을 공백 기준으로 split 하고, stream을 IntStream으로 변환하고, 다시 Array (배열)로 변환한다.
 
         Arrays.sort(answer);
 
