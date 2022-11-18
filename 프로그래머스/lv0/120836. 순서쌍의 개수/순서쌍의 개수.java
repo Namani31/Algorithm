29df9b86fbc8420b762c66e6e9139216d3a0db72
NaeRuMi <71462755+yunjikim-31@users.noreply.github.com>
2022-11-19 02:00:54 +0900

    [level 0] Title: 순서쌍의 개수, Time: 4.17 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -7,10 +7,9 @@ class Solution {
 
         for (int loop = 1; loop <= n; loop++) {
             if (n % loop == 0)  // n을 loop로 나누었을 때 나머지가 0 이면 순서쌍 해당
-                count++;
+                answer++;
         }
-
-        answer = count;
+        
         return answer;
     }
 }
