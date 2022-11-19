bef45c1e69b2fb8c13fcf679fd90d19c0a9d1783
Namani <71462755+yunjikim-31@users.noreply.github.com>
2022-11-19 21:24:46 +0900

    [level 1] Title: 약수의 합, Time: 0.05 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -1,21 +1,15 @@
-class Solution
-{
-    public int solution(int num) {
+class Solution {
+    public int solution(int n) {
         int answer = 0;
 
-        for (int loop = 1; loop <= Math.sqrt(num); loop++) {
-            // 나누어 떨어질 경우
-            if (num % loop == 0)
-            {
-                // n의 제곱근일 경우
-                if (loop * loop == num) {
-                    answer += loop;
-                }
-
-                // n의 제곱근이 아닐 경우
-                else {
-                    answer += loop;
-                    answer += num / loop;
+        for (int index = 1; index <= Math.sqrt(n); index++) {
+            if (n % index == 0) {
+                answer += index;
+                
+                int check = n / index;
+                
+                if (index != check) {
+                    answer += check;
                 }
             }
         }
