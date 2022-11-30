e73cd6077f287de95c10e2426fec3dd357150bf9
Namani <71462755+yunjikim-31@users.noreply.github.com>
2022-12-01 01:25:37 +0900

    [level 0] Title: 배열의 유사도, Time: 0.67 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
@@ -2,18 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] s1, String[] s2) {
-        int answer = 0;
-        int count = 0;
-
-        for (int firstArrayIndex = 0; firstArrayIndex < s1.length; firstArrayIndex++) {
-            for (int secondArrayIndex = 0; secondArrayIndex < s2.length; secondArrayIndex++) {
-                if (s1[firstArrayIndex].equals(s2[secondArrayIndex])) {
-                    count++;
-                }
-            }
-        }
-        answer = count;
-
-        return answer;
+        return (int) Arrays.stream(s1)
+                .filter(s1Element -> Arrays.asList(s2).contains(s1Element))
+                .count();
     }
 }
