0c2231f0d5e62eb2d9858dd14e6a5c7159cecc03
Namani <71462755+yunjikim-31@users.noreply.github.com>
2022-12-02 18:08:29 +0900

    [level 0] Title: 약수 구하기, Time: 2.23 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -2,24 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        int[] answer;
-        int j = 0;
-        int count = 0;
+        ArrayList<Integer> answer = new ArrayList<>();
 
-        for (int loop = 1; loop <= n; loop++) {
-            if (n % loop == 0) {
-                count++;
+        for (int index = 1; index <= n / 2; index++) {
+            if (n % index == 0) {
+                answer.add(index);
             }
         }
-        
-        answer = new int[count];
 
-        for (int loop2 = 1; loop2 <= n; loop2++) {
-            if (n % loop2 == 0) {
-                answer[j] = loop2;
-                j++;
-            }
-        }
-        return answer;
+        answer.add(n);
+
+        return answer.stream().mapToInt(element -> element).toArray();
     }
 }
