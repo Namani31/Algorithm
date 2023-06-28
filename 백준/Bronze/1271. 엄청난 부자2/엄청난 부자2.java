a349a35818ab9b5d6dbb1971b8ef24d863b110d5
Namani <71462755+Namani31@users.noreply.github.com>
2023-06-29 02:45:12 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 216 ms, Memory: 17760 KB -BaekjoonHub

--- 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -3,16 +3,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 문제를 잘 읽어보면 10의 1000제곱 이라는 제한이 있음, Int로 X, Long도 X
-        // BigInteger는 문자열로 되어있으므로 문자열을 인자 값으로 넘겨줘야 함
-        Scanner someMoney = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        BigInteger beakMoney = someMoney.nextBigInteger();
-        BigInteger giveMoney = someMoney.nextBigInteger();
+        BigInteger assistantMoney = sc.nextBigInteger();
+        BigInteger getPaid = sc.nextBigInteger();
 
-        someMoney.close();
-
-        System.out.println(beakMoney.divide(giveMoney));
-        System.out.println(beakMoney.remainder(giveMoney));
+        System.out.println(assistantMoney.divide(getPaid));
+        System.out.print(assistantMoney.remainder(getPaid));
     }
 }
