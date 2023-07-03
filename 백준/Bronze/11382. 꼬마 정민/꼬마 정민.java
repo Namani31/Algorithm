429e12f680e30926392a8f21519a8a72c8935d15
Namani <71462755+Namani31@users.noreply.github.com>
2023-07-03 23:12:46 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Long num1 = sc.nextLong();
+        Long num2 = sc.nextLong();
+        Long num3 = sc.nextLong();
+
+        System.out.print(num1 + num2 + num3);
+    }
+}
