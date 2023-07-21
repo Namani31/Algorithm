62d68552bc3175cc8f817113d63b310d6ef0ccfe
Namani <71462755+Namani31@users.noreply.github.com>
2023-07-21 15:38:57 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int studentNum = 0;
+        
         // 출석을 1부터 세므로 1~30이 필요함, 따라서 31 크기 배열 선언
         // boolean 타입은 선언하면서 false 로 초기화 됨
         boolean[] checkStudent = new boolean[31];
