72e565bd8150d776afdc06f355a7b080dda7479a
Namani <71462755+Namani31@users.noreply.github.com>
2023-07-25 23:17:56 +0900

    [Bronze I] Title: 더하기 사이클, Time: 216 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        scanner.close();
+
+        int cnt = 0;
+        int variableCopy = N;
+
+        while (true) {
+            N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+            cnt++;
+
+            if (variableCopy == N) {
+                break;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
