81e92f7c153d633571da61bf0316756ddc13e135
Namani <71462755+Namani31@users.noreply.github.com>
2024-02-20 11:25:18 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -4,14 +4,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-        int arr[] = new int[9];
+
         int max = 0;
         int index = 0;
         
-        for (int loop = 0; loop < arr.length; loop++) {
-            arr[loop] = Integer.parseInt(buffer.readLine());
-            if (arr[loop] > max) {
-                max = arr[loop];
+        for (int loop = 0; loop < 9; loop++) {
+            int value = Integer.parseInt(buffer.readLine());
+            
+            if (value > max) {
+                max = value;
                 index = loop + 1; // 배열 안의 index는 0부터 시작인데 몇 번째 수인지 구해야하므로 +1
             }
         }
