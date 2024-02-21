5449128901eba3659615807b8229e904f81c393d
Namani <71462755+Namani31@users.noreply.github.com>
2024-02-21 15:56:54 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 404 ms, Memory: 23268 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        scanner.close();
+        
+        for(int row = 1; row <= N; row++) {
+            for(int column = 1; column <= N - row; column++) {
+                System.out.print(" ");
+            }
+            for(int star = 0; star < row; star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
