4dd17e30738e9bd851d92bf84a9a55c9a1a6050a
Namani <71462755+Namani31@users.noreply.github.com>
2024-03-06 14:33:37 +0900

    [Bronze V] Title: 팩토리얼, Time: 200 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,21 +1,19 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int user_Num = scanner.nextInt();
+        int N = scanner.nextInt();
         scanner.close();
-
-        int fact_Sum = factorial(user_Num);
-        System.out.println(fact_Sum);
-    }
-
-    public static int factorial (int fact_Num) {
-        if (fact_Num <= 1) {
-            return 1;
+        
+        if (N < 2) {
+            System.out.println("1");
         } else {
-            return fact_Num * factorial(fact_Num - 1);
+            int fac = 1;
+            for (int index = 2; index <= N; index++) {
+                fac = fac * index;
+            }
+            System.out.println(fac);
         }
     }
 }
