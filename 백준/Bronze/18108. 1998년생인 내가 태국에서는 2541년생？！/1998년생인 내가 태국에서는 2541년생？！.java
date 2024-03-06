df1399f035292de69f0a9b54627756c0458c29b7
Namani <71462755+Namani31@users.noreply.github.com>
2024-03-06 13:12:33 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -1,18 +1,10 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int year = 0;
-
-        Scanner input = new Scanner(System.in);
-
-        year = input.nextInt();
-        input.close();
-
-        if (year < 1000 || year > 3000)
-            return;
-        else
-            year -= 543;
-        System.out.println(year);
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        
+        System.out.println(year - 543);
     }
 }
