20c09a8fd11919e29f8853cb27e148713f7001d6
Namani <71462755+Namani31@users.noreply.github.com>
2024-03-06 13:06:48 +0900

    [Bronze V] Title: 윤년, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,18 +1,15 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int year = 0;
-        year = scanner.nextInt();
-
-        if ((year % 4 == 0) && (year % 100 != 0)) {
-            System.out.println(1);
-        } else if (year % 400 == 0) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
+        int year = scanner.nextInt();
+        scanner.close();
+        
+        if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                System.out.println("1");
+            } else {
+                System.out.println("0");
         }
     }
 }
