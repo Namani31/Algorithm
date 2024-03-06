2962a41d911f083a8d44db477cdf4b7f3527fa0c
Namani <71462755+Namani31@users.noreply.github.com>
2024-03-06 10:23:12 +0900

    [Bronze V] Title: 나머지, Time: 204 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,21 +1,15 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int A = 0;
-        int B = 0;
-        int C = 0;
-
-        Scanner input = new Scanner(System.in);
-        A = input.nextInt();
-        B = input.nextInt();
-        C = input.nextInt();
-
-        input.close();
-
-        System.out.println( (A+B)%C );
-        System.out.println( (A%C + B%C)%C );
-        System.out.println( (A*B)%C );
-        System.out.println( (A%C * B%C)%C );
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+        
+        System.out.println((A+B) % C);
+        System.out.println(((A%C) + (B%C)) % C);
+        System.out.println((A*B) % C);
+        System.out.println(((A%C) * (B%C)) % C);
     }
 }
