86e9138c0f36e548d0c13083dd28a16650eccb70
Namani <71462755+Namani31@users.noreply.github.com>
2024-03-06 13:10:11 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17636 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,16 +1,11 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String userName = null;
-
-        Scanner input = new Scanner(System.in);
-
-        // nextLine: 문자열 입력
-        userName = input.nextLine();
-        input.close();
-
-        System.out.println(userName+"??!");
-
+        Scanner scanner = new Scanner(System.in);
+        String site = scanner.next();
+        scanner.close();
+        
+        System.out.println(site + "??!");
     }
 }
